e708b26bce8e58a24257f41d8d271e4be2dacca5
KDC <kdc7140@naver.com>
2023-03-10 18:13:00 +0900

    add page vue file

---
+++ frontend/src/views/Login/login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+		<h1>Sample Page</h1>
+    <button @click="apiTest">API 통신 테스트 type1</button><br/><br/>
+
+  </div>
+   
+</template>
+
+<script>
+
+import CommonUtil from '@/common/commonUtil.js';
+
+export default {
+  name: "Example",
+  data() {
+    return {
+      openActive : false,
+      testData : 0,
+    };
+  },
+  
+  computed: {
+   
+  },
+
+  watch:{
+
+  },
+
+  methods: {
+
+    async apiTest(){
+			console.log(rst);
+    },
+
+  },
+
+  mounted() {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/Popup/PopConfirm.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="popup-wrap show">
+        <div class="popup-bg"></div>
+        <div class="popup">
+            <!-- 타이틀 없을 경우 no-head -->
+            <div class="popup-box" :class="{'no-head' : popNoHead}">
+                <div class="pop-head">{{popTitle}}</div>
+                <div class="pop-body" v-html="popMsg">
+
+                </div>
+                <div class="pop-foot">
+                    <p class="btn-wrap">
+                        <button type="button" class="btn line" @click="popClick(false)">{{cancelButtonText}}</button>
+                        <button type="button" class="btn" @click="popClick(true)">{{popBtn}}</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'PopConfirm',
+  props: {
+    popNoHead:{
+      type: Boolean,
+      default: false
+    },
+    popTitle: {
+      type: String,
+      default: "",
+    },
+    popMsg: {
+      type: String,
+      default: "",
+    },
+    popBtn: {
+      type: String,
+      default: "확인",
+    },
+    cancelButtonText: {
+      type: String,
+      default: "취소",
+    },
+    isLeft : {
+      type: Boolean,
+      default : false
+    }
+  },
+  data() {
+    return {
+      PopConfirm: true,
+    };
+  },
+  methods : {
+    popClick: function(isTrueOrFalse) {
+      this.$emit("click", isTrueOrFalse) ;
+    },
+  }
+};
+</script>
