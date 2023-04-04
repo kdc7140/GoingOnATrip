0fa945629da96db2bfe94c9646dc3c4c4b2397bc
KDC <kdc7140@naver.com>
2023-04-04 15:34:10 +0900

    add common func

--- frontend/src/common/commonUtil.js
+++ frontend/src/common/commonUtil.js
@@ -1,6 +1,12 @@
-//공통함수 
-export default class CommonUtil{
-	static CommonFunc = () => {
-		console.log('common Test');
+var empty = require("is-empty");
+
+//공통함수
+export default class CommonUtil {
+  static CommonFunc = () => {
+    console.log("common Test");
+  };
+
+	static isEmpty = (data) => {
+		return empty(data);
 	}
 }
